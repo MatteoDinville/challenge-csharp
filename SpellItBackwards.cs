4f332e80ec0768e9b71d314ec5c0ef0316583f50
Matteo <matteo.dinville@ynov.com>
2022-01-04 16:27:11 +0100

    reverse word

---
+++ SpellItBackwards.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpDiscovery.Quest01
+{
+    public class SpellItBackwards_Exercice
+    {
+        public static string SpellItBackward(string str)
+        {
+            string result = "";
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
+            for (int i = str.Length - 1 ; i >= 0; i--)
+            {
+                result += str[i];
+            }
+            return result;
+        }
+    }
+}
