bf76b6a15865aeafb1028f25f974d8f5160783c9
Matteo <matteo.dinville@ynov.com>
2022-01-10 15:27:04 +0100

    inputTab

---
+++ DoubleArrayForEvenInteger.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharpDiscovery.Quest01
+{
+    public class DoubleArrayForEvenInteger_Exercice
+    {
+        public static int[] DoubleArrayForEvenInteger(int[] inputTab)
+        {
+            int num;
+            // parcourir ton array
+            for (int i = 0; i < inputTab.Length; i++)
+            {
+                // check si c pair ou impair
+               
+                if ( inputTab[i] % 2 != 0)
+                {
+                    num = inputTab[i] * 2;
+                }
+                else
+                {
+                    num = inputTab[i];
+                }
+                inputTab[i] = num;
+
+            }
+            // return le tab contenant les résultats
+            return inputTab;
+        }
+    }
+}
