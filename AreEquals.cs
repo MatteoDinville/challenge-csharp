103ceab9290c940547990d5585d41b41819e8448
Matteo <matteo.dinville@ynov.com>
2022-01-10 22:45:05 +0100

    compare array

---
+++ AreEquals.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpDiscovery.Quest02
+{
+    public class AreEquals_Exercice
+    {
+        public static bool AreEquals(int[] tab1, int[] tab2)
+        {
+            bool result = false;
+          if (tab1.Length == tab2.Length)
+            {
+                for (int i = 0; i < tab1.Length; i++)
+                {
+                    if (tab1[i] == tab2[i])
+                    {
+                        result = true;
+                    }else
+                    {
+                        result = false;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
