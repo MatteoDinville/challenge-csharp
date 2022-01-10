92e4acc0854f1b2830114ccf160b937aef7d4a99
Matteo <matteo.dinville@ynov.com>
2022-01-10 17:45:44 +0100

    parseExact

---
+++ FromStringToDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class FromStringToDateTime_Exercice
+    {
+        public static DateTime FromStringToDateTime(string dateStr)
+        {
+            DateTime dt = DateTime.ParseExact(dateStr, "dd/MM/yyyy HH:mm", null);
+            return dt;
+        }
+    }
+}  
