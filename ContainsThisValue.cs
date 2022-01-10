6c02562b96ef06ea559f36f79f0ec1387abe47eb
Matteo <matteo.dinville@ynov.com>
2022-01-10 21:24:44 +0100

    indexof

---
+++ ContainsThisValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class ContainsThisValue_Exercice
+    {
+        public static bool ContainsThisValue(int[] tab, int value)
+        {
+            if (Array.IndexOf(tab, value) == -1)   
+            {
+                return true;
+            }else
+            {
+                return false; 
+            }
+        }
+    }
+}
