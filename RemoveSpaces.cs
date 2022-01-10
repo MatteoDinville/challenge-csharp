b47886d3e256d4d8f0cf80907dcf88d835b369ac
Matteo <matteo.dinville@ynov.com>
2022-01-10 15:53:13 +0100

    trim

---
+++ RemoveSpaces.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpDiscovery.Quest01
+{
+    public class RemoveSpaces_Exercice
+    {
+        public static string RemoveSpaces(string str)
+        {
+            if (str == null)
+            {
+                return "0";
+            }
+            return str.Trim();
+        }
+    }
+}
