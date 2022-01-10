547081380e07b03597e0d4c4d33f808720e79910
Matteo <matteo.dinville@ynov.com>
2022-01-10 16:46:01 +0100

    date

---
+++ TodayAtMidnight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class TodayAtMidnight_Exercice
+    {
+        public static DateTime TodayAtMidnight()
+        {
+            DateTime endTime = DateTime.Now;
+            return endTime.Subtract(endTime.TimeOfDay);
+        }
+    }
+}
