66f7c426e51e81997cb67d5cf30c0f2cca24682d
Matteo <matteo.dinville@ynov.com>
2022-01-10 23:46:24 +0100

    lastindex

---
+++ FindLastIndex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class FindLastIndex_Exercice
+    {
+        public static int? FindLastIndex(int[] tab, int a)
+        {
+           if (tab == null)
+            {
+                return null; 
+            }
+            int lastIndex = Array.LastIndexOf(tab, a);
+            return lastIndex > 0 ? lastIndex : null;
+        }
+    }
+}
