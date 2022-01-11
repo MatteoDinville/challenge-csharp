c5ec1047b3ca4f1e39a0e072f2fc0c6161f7d625
Matteo <matteo.dinville@ynov.com>
2022-01-11 15:55:44 +0100

    distance

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -35,5 +35,26 @@ namespace CSharpDiscovery.Quest03
             string resultTemplate = "{0} (Lat={1}, Long={2})";
                 return string.Format(resultTemplate, this.Name, this.Latitude, this.Longitude);
         }
+
+        private static double GetDistancePriv(double longitude, double latitude, double otherLongitude, double otherLatitude)
+        {
+            var d1 = latitude * (Math.PI / 180.0);
+            var num1 = longitude * (Math.PI / 180.0);
+            var d2 = otherLatitude * (Math.PI / 180.0);
+            var num2 = otherLongitude * (Math.PI / 180.0) - num1;
+            var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
+
+            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+        }
+
+        public int GetDistance(PointOfInterest other)
+        {
+            return (int)Math.Round(GetDistancePriv(this.Longitude, this.Latitude, other.Longitude, other.Latitude) * 0.001);
+        }
+
+        public static int GetDistance(PointOfInterest p1, PointOfInterest p2)
+        {
+            return (int)Math.Round(GetDistancePriv(p1.Longitude, p1.Latitude, p2.Longitude, p2.Latitude) * 0.001);
+        }
     }
 }
