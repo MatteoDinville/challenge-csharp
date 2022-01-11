51e895b41acd6b160f0f35ea13105493d3959929
Matteo <matteo.dinville@ynov.com>
2022-01-11 16:10:41 +0100

    héritage

---
+++ Campus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest03
+{
+    public class Campus : PointOfInterest
+    {
+        public int Capacity 
+        {
+            get; set;
+        }
+    public override string ToString()
+    {
+        string Template = "{0} is a campus with a capacity of {1} students";
+        return string.Format(Template, this.Name, Capacity);
+    }
+    }
+}
