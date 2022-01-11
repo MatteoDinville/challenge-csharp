03ba82ac9ff207ffff022aa35a2e084e3add0c34
Matteo <matteo.dinville@ynov.com>
2022-01-11 15:06:17 +0100

    constructor

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -9,5 +9,18 @@ namespace CSharpDiscovery.Quest03
         public string Name { get; set; }
         public static string GoogleMapsUrlTemplate { get; set; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
 
+        public PointOfInterest()
+        {
+            this.Name = "Bordeaux Ynov Campus";
+            this.Latitude = 44.854186;
+            this.Longitude = -0.5663056;
+        }
+
+        public PointOfInterest(string Name, double Latitude, double Longitude)
+        {
+            this.Name = Name;
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+        }
     }
 }
