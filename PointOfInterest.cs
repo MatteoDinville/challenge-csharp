6ead82e9df5c31ad2af7f8b037df48871c4bee9f
Matteo <matteo.dinville@ynov.com>
2022-01-11 14:53:09 +0100

    point

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -4,10 +4,10 @@ namespace CSharpDiscovery.Quest03
 {
     public class PointOfInterest
     {
-        public double Latitude;
-        public double Longitude;
-        public double Name;
-        public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Name { get; set; }
+        public static string GoogleMapsUrlTemplate { get; set; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
 
     }
 }
