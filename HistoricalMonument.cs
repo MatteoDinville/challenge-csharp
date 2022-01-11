25d827ba32edd7cf6a3feed1a0711fbb1ab4909e
Matteo <matteo.dinville@ynov.com>
2022-01-11 16:35:37 +0100

    distance

---
+++ HistoricalMonument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest03
+{
+    public class HistoricalMonument : PointOfInterest
+    {
+        public int BuildYear{ get; set; }
+
+        public HistoricalMonument() { }
+
+        public HistoricalMonument(double Lat, double Long, string Name, int Build) : base(Lat, Long, Name)
+        {
+            this.BuildYear = Build;
+        }
+        public override string ToString()
+        {
+            string Template = "{0} is a historical monument built in {1}";
+            return string.Format(Template, this.Name, this.BuildYear);
+        }
+    }
+}
