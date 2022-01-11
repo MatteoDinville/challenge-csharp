e32baad77b874b9625b120e77dc2a1e7683a3b71
Matteo <matteo.dinville@ynov.com>
2022-01-11 17:06:11 +0100

    car

--- Car.cs
+++ Car.cs
@@ -1,10 +1,24 @@
 ﻿using System;
-namespace CSharpDiscovery
+
+namespace CSharpDiscovery.Quest04
 {
-    public class Car
+    public class Car : Vehicule
     {
-        public Car()
+        public string Model { get; set; }
+
+        public Car() : base()
         {
+            Brand = "Unknown";
+        }
+
+        public Car(string Color, string Brand, string Model, int CurrentSpeed = 0) : base(Brand, Color, CurrentSpeed)
+        {
+            this.Model = Model;
+        }
+        public override string ToString()
+        {
+            string Template = "{0} {1} {2}";
+            return string.Format(Template, this.Color, this.Brand, this.Model);
         }
     }
 }
