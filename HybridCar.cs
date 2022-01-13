60e4fbc055546dc774c344e2946946d2b48cd7f5
Matteo <matteo.dinville@ynov.com>
2022-01-13 16:01:30 +0100

    hybrid

---
+++ HybridCar.cs
@@ -0,0 +1,56 @@
+﻿namespace CSharpDiscovery.Quest04
+{
+    public class HybridCar : Car, IElectricCar, IThermalCar
+    {
+
+        public int FuelLevel
+        {
+            get;
+            set;
+        }
+
+        public int BatteryLevel
+        {
+            get;
+            set;
+        }
+
+        public HybridCar() : base()
+        {
+            FuelLevel = 100;
+            BatteryLevel = 100;
+        }
+
+        public HybridCar(string Model, string Brand, string Color, int CurrentSpeed = 0) : base(Model, Brand, Color, CurrentSpeed)
+        {
+            FuelLevel = 100;
+            BatteryLevel = 100;
+        }
+
+        public void FillUp()
+        {
+            FuelLevel = 100;
+        }
+
+        public int GetFuelLevel()
+        {
+            return FuelLevel;
+        }
+
+        public void Recharge()
+        {
+            BatteryLevel = 100;
+        }
+
+        public int GetBatteryLevel()
+        {
+            return BatteryLevel;
+        }
+
+        public override string ToString()
+        {
+            string strTemplate = "{0} {1} {2}, Battery: {3}%, Fuel: {4}%";
+            return string.Format(strTemplate, this.Color, this.Brand, this.Model, this.BatteryLevel, this.FuelLevel);
+        }
+    }
+}
